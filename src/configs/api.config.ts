daaacfef4aeb5fa608065e88f1df52df5bff147b
bhaktibuana <bhaktibuana19@gmail.com>
2023-05-25 23:12:30 +0700

    fix bug api url

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -7,11 +7,11 @@ const apiUrl = (): apiUrlInface => {
   return {
     apiV1:
       process.env.NODE_ENV === "production"
-        ? "https://api-portfolio.bhaktibuana.com//api/v1"
+        ? "https://api-portfolio.bhaktibuana.com/api/v1"
         : "http://localhost:3001/api/v1",
     apiV2:
       process.env.NODE_ENV === "production"
-        ? "https://api-portfolio.bhaktibuana.com//api/v2"
+        ? "https://api-portfolio.bhaktibuana.com/api/v2"
         : "http://localhost:3001/api/v2",
   };
 };
